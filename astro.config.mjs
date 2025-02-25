15ad24cc4de6fb91219b92ce9f6d73da99e290ab
Victor Toh <victortoh@gmail.com>
2025-02-25 18:07:28 +0900

    Revert to bun and add path resolution to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,27 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import icon from "astro-icon";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   // Configure Astro here
   site: "https://japanccc.netlify.app",
   vite: {
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./src"),
+      },
+    },
     plugins: [tailwindcss()],
   },
-  integrations: [icon()],
+  integrations: [
+    icon({
+      include: {
+        ph: ["*"], // all Phosphor icons
+      },
+    }),
+  ],
 });
