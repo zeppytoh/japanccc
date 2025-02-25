8403471e818ae31da274b6527da044b110f43687
Victor Toh <victortoh@gmail.com>
2025-02-25 21:57:42 +0900

    Add second page for students

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,12 @@ import { defineConfig } from "astro/config";
 import path from "path";
 import { fileURLToPath } from "url";
 
-import icon from "astro-icon";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   // Configure Astro here
   site: "https://japanccc.netlify.app",
   vite: {
-    resolve: {
-      alias: {
-        "~": path.resolve(__dirname, "./src"),
-      },
-    },
     plugins: [tailwindcss()],
   },
-  integrations: [
-    icon({
-      include: {
-        ph: ["*"], // all Phosphor icons
-      },
-    }),
-  ],
 });
